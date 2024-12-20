8caaa1bba5d09da55fe39361ae6a96b72369b56f
topheryoung <cryoung488@gmail.com>
2024-12-20 15:57:59 +0100

    build: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+  output: 'server',
+  adapter: vercel(),
 });
